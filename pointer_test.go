3a75d249eac162409ce55d5333b03db931d55744
Adrian Witas <adrianwit@gmail.com>
2021-12-25 09:47:58 -0800

    - removed ValuePointerForType use AsPointer instead - moved tests

--- pointer_test.go
+++ pointer_test.go
@@ -5,9 +5,108 @@ import (
 	"reflect"
 	"testing"
 	"time"
+	"unsafe"
 )
 
-func TestFieldPointer(t *testing.T) {
+func TesAsPointer(t *testing.T) {
+	k := 10
+	l := 20
+	i := int64(30)
+	type Foo struct {
+		ID   int
+		Name string
+	}
+	var testCases = []struct {
+		description string
+		t           reflect.Type
+		value       interface{}
+		castBack    func(pointer unsafe.Pointer) interface{}
+	}{
+
+		{
+			description: "int",
+			t:           reflect.TypeOf(0),
+			value:       10,
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*int)(pointer)
+			},
+		},
+		{
+			description: "*int64",
+			t:           reflect.TypeOf(&i),
+			value:       &i,
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(**int64)(pointer)
+			},
+		},
+		{
+			description: "[]*int",
+			t:           reflect.TypeOf([]*int{}),
+			value:       []*int{&k, &l},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*[]*int)(pointer)
+			},
+		},
+		{
+			description: "[]byte",
+			t:           reflect.TypeOf([]byte{}),
+			value:       []byte{1, 2, 3},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*[]byte)(pointer)
+			},
+		},
+		{
+			description: "struct ",
+			t:           reflect.TypeOf(Foo{}),
+			value:       Foo{ID: 1, Name: "abc"},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*Foo)(pointer)
+			},
+		},
+		{
+			description: "*struct ",
+			t:           reflect.TypeOf(&Foo{}),
+			value:       &Foo{ID: 1, Name: "abc"},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(**Foo)(pointer)
+			},
+		},
+		{
+			description: "[]struct ",
+			t:           reflect.TypeOf([]Foo{}),
+			value:       []Foo{{ID: 1, Name: "abc"}},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*[]Foo)(pointer)
+			},
+		},
+		{
+			description: "[]*struct ",
+			t:           reflect.TypeOf([]*Foo{}),
+			value:       []*Foo{{ID: 1, Name: "abc"}},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*[]*Foo)(pointer)
+			},
+		},
+		{
+			description: "[]*struct ",
+			t:           reflect.TypeOf([]interface{}{}),
+			value:       []interface{}{&Foo{ID: 1, Name: "abc"}},
+			castBack: func(pointer unsafe.Pointer) interface{} {
+				return *(*[]interface{})(pointer)
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		ptr := reflect.New(testCase.t)
+		ptr.Elem().Set(reflect.ValueOf(testCase.value))
+		v := AsPointer(ptr.Interface())
+		actual := testCase.castBack(v)
+		assert.EqualValues(t, testCase.value, actual, testCase.description)
+	}
+}
+
+func TestEnsurePointer(t *testing.T) {
 
 	var testCases = []struct {
 		description string
