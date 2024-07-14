e12bc4bb9c13423289db319e2b7f8cbde0f9ae1b
adranwit <adrianwit@gmail.com>
2024-07-13 20:19:12 -0700

    patched field address assertion

--- accessor.go
+++ accessor.go
@@ -6,7 +6,7 @@ import (
 	"unsafe"
 )
 
-//Interface cast field pointer to value
+// Interface cast field pointer to value
 func (f *Field) Interface(structPtr unsafe.Pointer) interface{} {
 	ptr := f.Pointer(structPtr)
 	if f.ptrKind == ptrKindEmptyInterface {
@@ -20,13 +20,13 @@ func (f *Field) Interface(structPtr unsafe.Pointer) interface{} {
 	return *(*interface{})(ptr)
 }
 
-//Int cast field pointer to int
+// Int cast field pointer to int
 func (f *Field) Int(structPtr unsafe.Pointer) int {
 	f.MustBeAssignable(xreflect.IntType)
 	return AsInt(f.Pointer(structPtr))
 }
 
-//IntPtr cast field pointer to *int
+// IntPtr cast field pointer to *int
 func (f *Field) IntPtr(structPtr unsafe.Pointer) *int {
 	f.MustBeAssignable(xreflect.IntPtrType)
 	result := AsIntAddrPtr(f.Pointer(structPtr))
@@ -36,13 +36,13 @@ func (f *Field) IntPtr(structPtr unsafe.Pointer) *int {
 	return *result
 }
 
-//Error cast field pointer to error
+// Error cast field pointer to error
 func (f *Field) Error(structPtr unsafe.Pointer) error {
 	f.MustBeAssignable(xreflect.ErrorType)
 	return AsError(f.Pointer(structPtr))
 }
 
-//ErrorPtr cast field pointer to *error
+// ErrorPtr cast field pointer to *error
 func (f *Field) ErrorPtr(structPtr unsafe.Pointer) *error {
 	f.MustBeAssignable(xreflect.ErrorPtrType)
 	result := AsErrorAddrPtr(f.Pointer(structPtr))
@@ -52,19 +52,19 @@ func (f *Field) ErrorPtr(structPtr unsafe.Pointer) *error {
 	return *result
 }
 
-//IntAddr cast field pointer to **int
+// IntAddr cast field pointer to **int
 func (f *Field) IntAddr(structPtr unsafe.Pointer) *int {
-	f.MustBeAssignable(xreflect.IntPtrType)
+	f.MustBeAssignable(xreflect.IntType)
 	return AsIntPtr(f.Pointer(structPtr))
 }
 
-//Int64 cast field pointer to int64
+// Int64 cast field pointer to int64
 func (f *Field) Int64(structPtr unsafe.Pointer) int64 {
 	f.MustBeAssignable(xreflect.Int64Type)
 	return AsInt64(f.Pointer(structPtr))
 }
 
-//Int64Ptr cast field pointer to *int
+// Int64Ptr cast field pointer to *int
 func (f *Field) Int64Ptr(structPtr unsafe.Pointer) *int64 {
 	f.MustBeAssignable(xreflect.Int64PtrType)
 	result := AsInt64AddrPtr(f.Pointer(structPtr))
@@ -74,19 +74,19 @@ func (f *Field) Int64Ptr(structPtr unsafe.Pointer) *int64 {
 	return *result
 }
 
-//Int64Addr cast field pointer to *int64
+// Int64Addr cast field pointer to *int64
 func (f *Field) Int64Addr(structPtr unsafe.Pointer) *int64 {
-	f.MustBeAssignable(xreflect.Int64PtrType)
+	f.MustBeAssignable(xreflect.Int64Type)
 	return AsInt64Ptr(f.Pointer(structPtr))
 }
 
-//Int32 cast field pointer to int32
+// Int32 cast field pointer to int32
 func (f *Field) Int32(structPtr unsafe.Pointer) int32 {
 	f.MustBeAssignable(xreflect.Int32Type)
 	return AsInt32(f.Pointer(structPtr))
 }
 
-//Int32Ptr cast field pointer to *int32
+// Int32Ptr cast field pointer to *int32
 func (f *Field) Int32Ptr(structPtr unsafe.Pointer) *int32 {
 	f.MustBeAssignable(xreflect.Int32PtrType)
 	result := AsInt32AddrPtr(f.Pointer(structPtr))
@@ -96,19 +96,19 @@ func (f *Field) Int32Ptr(structPtr unsafe.Pointer) *int32 {
 	return *result
 }
 
-//Int32Addr cast field pointer to *int32
+// Int32Addr cast field pointer to *int32
 func (f *Field) Int32Addr(structPtr unsafe.Pointer) *int32 {
-	f.MustBeAssignable(xreflect.Int32PtrType)
+	f.MustBeAssignable(xreflect.Int32Type)
 	return AsInt32Ptr(f.Pointer(structPtr))
 }
 
-//Int16 cast field pointer to int16
+// Int16 cast field pointer to int16
 func (f *Field) Int16(structPtr unsafe.Pointer) int16 {
 	f.MustBeAssignable(xreflect.Int16Type)
 	return AsInt16(f.Pointer(structPtr))
 }
 
-//Int16Ptr cast field pointer to *int16
+// Int16Ptr cast field pointer to *int16
 func (f *Field) Int16Ptr(structPtr unsafe.Pointer) *int16 {
 	f.MustBeAssignable(xreflect.Int16PtrType)
 	result := AsInt16AddrPtr(f.Pointer(structPtr))
@@ -118,19 +118,19 @@ func (f *Field) Int16Ptr(structPtr unsafe.Pointer) *int16 {
 	return *result
 }
 
-//Int16Addr returns *int16
+// Int16Addr returns *int16
 func (f *Field) Int16Addr(structPtr unsafe.Pointer) *int16 {
-	f.MustBeAssignable(xreflect.Int16PtrType)
+	f.MustBeAssignable(xreflect.Int16Type)
 	return AsInt16Ptr(f.Pointer(structPtr))
 }
 
-//Int8 cast field pointer to int8
+// Int8 cast field pointer to int8
 func (f *Field) Int8(structPtr unsafe.Pointer) int8 {
 	f.MustBeAssignable(xreflect.Int8Type)
 	return AsInt8(f.Pointer(structPtr))
 }
 
-//Int8Ptr cast field pointer to *int8
+// Int8Ptr cast field pointer to *int8
 func (f *Field) Int8Ptr(structPtr unsafe.Pointer) *int8 {
 	f.MustBeAssignable(xreflect.Int8PtrType)
 	result := AsInt8AddrPtr(f.Pointer(structPtr))
@@ -140,25 +140,25 @@ func (f *Field) Int8Ptr(structPtr unsafe.Pointer) *int8 {
 	return *result
 }
 
-//Int8Addr cast field pointer to *int8
+// Int8Addr cast field pointer to *int8
 func (f *Field) Int8Addr(structPtr unsafe.Pointer) *int8 {
-	f.MustBeAssignable(xreflect.Int8PtrType)
+	f.MustBeAssignable(xreflect.Int8Type)
 	return AsInt8Ptr(f.Pointer(structPtr))
 }
 
-//UintAddr cast field pointer to **uint
+// UintAddr cast field pointer to **uint
 func (f *Field) UintAddr(structPtr unsafe.Pointer) *uint {
-	f.MustBeAssignable(xreflect.UintPtrType)
+	f.MustBeAssignable(xreflect.UintType)
 	return AsUintPtr(f.Pointer(structPtr))
 }
 
-//Uint cast field pointer to uint
+// Uint cast field pointer to uint
 func (f *Field) Uint(structPtr unsafe.Pointer) uint {
 	f.MustBeAssignable(xreflect.UintType)
 	return AsUint(f.Pointer(structPtr))
 }
 
-//UintPtr cast field pointer to *uint
+// UintPtr cast field pointer to *uint
 func (f *Field) UintPtr(structPtr unsafe.Pointer) *uint {
 	f.MustBeAssignable(xreflect.UintPtrType)
 	result := AsUintAddrPtr(f.Pointer(structPtr))
@@ -168,13 +168,13 @@ func (f *Field) UintPtr(structPtr unsafe.Pointer) *uint {
 	return *result
 }
 
-//Uint64 cast field pointer to uint64
+// Uint64 cast field pointer to uint64
 func (f *Field) Uint64(structPtr unsafe.Pointer) uint64 {
 	f.MustBeAssignable(xreflect.Uint64Type)
 	return AsUint64(f.Pointer(structPtr))
 }
 
-//Uint64Ptr cast field pointer to *uint64
+// Uint64Ptr cast field pointer to *uint64
 func (f *Field) Uint64Ptr(structPtr unsafe.Pointer) *uint64 {
 	f.MustBeAssignable(xreflect.Uint64PtrType)
 	result := AsUint64AddrPtr(f.Pointer(structPtr))
@@ -184,19 +184,19 @@ func (f *Field) Uint64Ptr(structPtr unsafe.Pointer) *uint64 {
 	return *result
 }
 
-//Uint64Addr cast field pointer to *uint64
+// Uint64Addr cast field pointer to *uint64
 func (f *Field) Uint64Addr(structPtr unsafe.Pointer) *uint64 {
-	f.MustBeAssignable(xreflect.Uint64PtrType)
+	f.MustBeAssignable(xreflect.Uint64Type)
 	return AsUint64Ptr(f.Pointer(structPtr))
 }
 
-//Uint32 cast field pointer to uint32
+// Uint32 cast field pointer to uint32
 func (f *Field) Uint32(structPtr unsafe.Pointer) uint32 {
 	f.MustBeAssignable(xreflect.Uint32Type)
 	return AsUint32(f.Pointer(structPtr))
 }
 
-//Uint32Ptr cast field pointer to *uint32
+// Uint32Ptr cast field pointer to *uint32
 func (f *Field) Uint32Ptr(structPtr unsafe.Pointer) *uint32 {
 	f.MustBeAssignable(xreflect.Uint32PtrType)
 	result := AsUint32AddrPtr(f.Pointer(structPtr))
@@ -206,19 +206,19 @@ func (f *Field) Uint32Ptr(structPtr unsafe.Pointer) *uint32 {
 	return *result
 }
 
-//Uint32Addr cast field pointer to *uint32
+// Uint32Addr cast field pointer to *uint32
 func (f *Field) Uint32Addr(structPtr unsafe.Pointer) *uint32 {
-	f.MustBeAssignable(xreflect.Uint32PtrType)
+	f.MustBeAssignable(xreflect.Uint32Type)
 	return AsUint32Ptr(f.Pointer(structPtr))
 }
 
-//Uint16 cast field pointer to uint16
+// Uint16 cast field pointer to uint16
 func (f *Field) Uint16(structPtr unsafe.Pointer) uint16 {
 	f.MustBeAssignable(xreflect.Uint16Type)
 	return AsUint16(f.Pointer(structPtr))
 }
 
-//Uint16Ptr cast field pointer to *uint16
+// Uint16Ptr cast field pointer to *uint16
 func (f *Field) Uint16Ptr(structPtr unsafe.Pointer) *uint16 {
 	f.MustBeAssignable(xreflect.Uint16PtrType)
 	result := AsUint16AddrPtr(f.Pointer(structPtr))
@@ -228,19 +228,19 @@ func (f *Field) Uint16Ptr(structPtr unsafe.Pointer) *uint16 {
 	return *result
 }
 
-//Uint16Addr cast field pointer to *uint16
+// Uint16Addr cast field pointer to *uint16
 func (f *Field) Uint16Addr(structPtr unsafe.Pointer) *uint16 {
-	f.MustBeAssignable(xreflect.Uint16PtrType)
+	f.MustBeAssignable(xreflect.Uint16Type)
 	return AsUint16Ptr(f.Pointer(structPtr))
 }
 
-//Uint8 cast field pointer to uint8
+// Uint8 cast field pointer to uint8
 func (f *Field) Uint8(structPtr unsafe.Pointer) uint8 {
 	f.MustBeAssignable(xreflect.Uint8Type)
 	return AsUint8(f.Pointer(structPtr))
 }
 
-//Uint8Ptr cast field pointer to *uint8
+// Uint8Ptr cast field pointer to *uint8
 func (f *Field) Uint8Ptr(structPtr unsafe.Pointer) *uint8 {
 	f.MustBeAssignable(xreflect.Uint8PtrType)
 	result := AsUint8AddrPtr(f.Pointer(structPtr))
@@ -250,19 +250,19 @@ func (f *Field) Uint8Ptr(structPtr unsafe.Pointer) *uint8 {
 	return *result
 }
 
-//Uint8Addr cast field pointer to *uint8
+// Uint8Addr cast field pointer to *uint8
 func (f *Field) Uint8Addr(structPtr unsafe.Pointer) *uint8 {
-	f.MustBeAssignable(xreflect.Uint8PtrType)
+	f.MustBeAssignable(xreflect.Uint8Type)
 	return AsUint8Ptr(f.Pointer(structPtr))
 }
 
-//Bool cast field pointer to bool
+// Bool cast field pointer to bool
 func (f *Field) Bool(structPtr unsafe.Pointer) bool {
 	f.MustBeAssignable(xreflect.BoolType)
 	return AsBool(f.Pointer(structPtr))
 }
 
-//BoolPtr cast field pointer to *bool
+// BoolPtr cast field pointer to *bool
 func (f *Field) BoolPtr(structPtr unsafe.Pointer) *bool {
 	f.MustBeAssignable(xreflect.BoolPtrType)
 	result := AsBoolAddrPtr(f.Pointer(structPtr))
@@ -272,19 +272,19 @@ func (f *Field) BoolPtr(structPtr unsafe.Pointer) *bool {
 	return *result
 }
 
-//BoolAddr cast field pointer to **bool
+// BoolAddr cast field pointer to **bool
 func (f *Field) BoolAddr(structPtr unsafe.Pointer) *bool {
-	f.MustBeAssignable(xreflect.BoolPtrType)
+	f.MustBeAssignable(xreflect.BoolType)
 	return AsBoolPtr(f.Pointer(structPtr))
 }
 
-//Float64 cast field pointer to float64
+// Float64 cast field pointer to float64
 func (f *Field) Float64(structPtr unsafe.Pointer) float64 {
 	f.MustBeAssignable(xreflect.Float64Type)
 	return AsFloat64(f.Pointer(structPtr))
 }
 
-//Float64Ptr cast field pointer to *float64
+// Float64Ptr cast field pointer to *float64
 func (f *Field) Float64Ptr(structPtr unsafe.Pointer) *float64 {
 	f.MustBeAssignable(xreflect.Float64PtrType)
 	result := AsFloat64AddrPtr(f.Pointer(structPtr))
@@ -294,19 +294,19 @@ func (f *Field) Float64Ptr(structPtr unsafe.Pointer) *float64 {
 	return *result
 }
 
-//Float64Addr cast field pointer to *float64
+// Float64Addr cast field pointer to *float64
 func (f *Field) Float64Addr(structPtr unsafe.Pointer) *float64 {
-	f.MustBeAssignable(xreflect.Float64PtrType)
+	f.MustBeAssignable(xreflect.Float64Type)
 	return AsFloat64Ptr(f.Pointer(structPtr))
 }
 
-//Float32 cast field pointer to float32
+// Float32 cast field pointer to float32
 func (f *Field) Float32(structPtr unsafe.Pointer) float32 {
 	f.MustBeAssignable(xreflect.Float32Type)
 	return AsFloat32(f.Pointer(structPtr))
 }
 
-//Float32Ptr cast field pointer to *float32
+// Float32Ptr cast field pointer to *float32
 func (f *Field) Float32Ptr(structPtr unsafe.Pointer) *float32 {
 	f.MustBeAssignable(xreflect.Float32PtrType)
 	result := AsFloat32AddrPtr(f.Pointer(structPtr))
@@ -316,19 +316,19 @@ func (f *Field) Float32Ptr(structPtr unsafe.Pointer) *float32 {
 	return *result
 }
 
-//Float32Addr cast field pointer to *float32
+// Float32Addr cast field pointer to *float32
 func (f *Field) Float32Addr(structPtr unsafe.Pointer) *float32 {
-	f.MustBeAssignable(xreflect.Float32PtrType)
+	f.MustBeAssignable(xreflect.Float32Type)
 	return AsFloat32Ptr(f.Pointer(structPtr))
 }
 
-//String cast field pointer to string
+// String cast field pointer to string
 func (f *Field) String(structPtr unsafe.Pointer) string {
 	f.MustBeAssignable(xreflect.StringType)
 	return AsString(f.Pointer(structPtr))
 }
 
-//StringPtr cast field pointer to *string
+// StringPtr cast field pointer to *string
 func (f *Field) StringPtr(structPtr unsafe.Pointer) *string {
 	f.MustBeAssignable(xreflect.StringPtrType)
 	result := AsStringAddrPtr(f.Pointer(structPtr))
@@ -338,19 +338,19 @@ func (f *Field) StringPtr(structPtr unsafe.Pointer) *string {
 	return *result
 }
 
-//StringAddr field pointer to *string
+// StringAddr field pointer to *string
 func (f *Field) StringAddr(structPtr unsafe.Pointer) *string {
-	f.MustBeAssignable(xreflect.StringPtrType)
+	f.MustBeAssignable(xreflect.StringType)
 	return AsStringPtr(f.Pointer(structPtr))
 }
 
-//Bytes cast field pointer to []byte
+// Bytes cast field pointer to []byte
 func (f *Field) Bytes(structPtr unsafe.Pointer) []byte {
 	f.MustBeAssignable(xreflect.BytesType)
 	return AsUint8s(f.Pointer(structPtr))
 }
 
-//BytesPtr cast field pointer to *[]byte
+// BytesPtr cast field pointer to *[]byte
 func (f *Field) BytesPtr(structPtr unsafe.Pointer) *[]byte {
 	f.MustBeAssignable(xreflect.BytesPtrType)
 	result := AsBytesAddrPtr(f.Pointer(structPtr))
@@ -360,7 +360,7 @@ func (f *Field) BytesPtr(structPtr unsafe.Pointer) *[]byte {
 	return *result
 }
 
-//TimePtr cast field pointer to *time.Time
+// TimePtr cast field pointer to *time.Time
 func (f *Field) TimePtr(structPtr unsafe.Pointer) *time.Time {
 	f.MustBeAssignable(xreflect.TimePtrType)
 	result := AsTimeAddrPtr(f.Pointer(structPtr))
@@ -370,13 +370,13 @@ func (f *Field) TimePtr(structPtr unsafe.Pointer) *time.Time {
 	return *result
 }
 
-//Time cast field pointer to time.Time
+// Time cast field pointer to time.Time
 func (f *Field) Time(structPtr unsafe.Pointer) time.Time {
 	f.MustBeAssignable(xreflect.TimeType)
 	return AsTime(f.Pointer(structPtr))
 }
 
-//Value returns field value
+// Value returns field value
 func (f *Field) Value(structPtr unsafe.Pointer) interface{} {
 	return f.Interface(structPtr)
 }
